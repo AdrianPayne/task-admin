a96602b0044d394ff05d2f90f31b4e43fe6d9667
AdrianPayne <adriansacristan1993@gmail.com>
2022-05-18 14:42:04 +0100

    sort code

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+function Header(){
+  
+    return (
+      <header>
+        <div id="home">
+          <img id="imgIcon" src="https://senniors.com/wp-content/uploads/sites/2/2020/07/logo-senniors.svg" alt="Senniors icon"/>
+          <h1>Sennior Team</h1>
+        </div>
+        <div id="profile">
+          <img id="profileImg" src="https://media-exp1.licdn.com/dms/image/C4D03AQGnXQ7abV2wzQ/profile-displayphoto-shrink_800_800/0/1609286932254?e=1658361600&v=beta&t=z5Tkw1X9NoraO4w9WnxVqfCqLlEubiAvisGnGb1P1ug" alt="Adrian"/>
+          <p>Adrian</p>
+        </div>
+      </header>
+    );
+  }
+
+export default Header;
